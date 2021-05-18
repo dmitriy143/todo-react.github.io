d9d92c9def047dff2be8326c881c06ea57c9dda8
Дмитрий <dima1986_09@list.ru>
2021-05-18 20:53:37 +0300

    create component Tasks

---
+++ src/components/Tasks/Tasks.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Tasks() {
+  return (
+    <div className="todo__warpper-tasks tasks">
+      <h2 className="tasks__title">Фронтенд</h2>
+    </div>
+  )
+}
+export default Tasks;
